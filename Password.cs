7643834b2fbedd0fdbe5b57d8ef481d0a0a69cf8
Vilipi <91099483+Vilipi@users.noreply.github.com>
2021-12-27 19:25:16 +0100

    Main part of the API

---
+++ Password.cs
@@ -0,0 +1,14 @@
+﻿namespace AA1Cliente {
+    public class Password {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string User { get; set; }
+        public string Date { get; set; }
+        public string Url { get; set; }
+        public string TagName { get; set; }
+        public string Pass { get; set; }
+        public string Description { get; set; }
+
+    }
+
+}
